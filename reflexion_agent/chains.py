e5e3fa4c3356c426748d381384a81b67c90085e0
JojuGeorge <jojugeorge001@gmail.com>
2026-05-26 15:25:33 +0530

    revisor agent added

--- reflexion_agent/chains.py
+++ reflexion_agent/chains.py
@@ -48,18 +48,18 @@ first_responder_prompt_template = actor_prompt_template.partial(
 #     tools=[AnswerQuestion], tool_choice="AnswerQuestion"
 # )
 
-# revise_instructions = """Revise your previous answer using the new information.
-#     - You should use the previous critique to add important information to your answer.
-#         - You MUST include numerical citations in your revised answer to ensure it can be verified.
-#         - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
-#             - [1] https://example.com
-#             - [2] https://example.com
-#     - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
-# """
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
 
-# revisor = actor_prompt_template.partial(
-#     first_instruction=revise_instructions
-# ) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
+revisor = actor_prompt_template.partial(
+    first_instruction=revise_instructions
+) | llm.with_structured_output(ReviseAnswer)
 
 
 if __name__ == "__main__":
