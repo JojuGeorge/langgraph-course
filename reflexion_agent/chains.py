d7b00d63818be01ddc852581f1c1b7d7945bde3b
JojuGeorge <jojugeorge001@gmail.com>
2026-05-26 16:50:35 +0530

    main added shcema use in chains changes. flow.png added

--- reflexion_agent/chains.py
+++ reflexion_agent/chains.py
@@ -7,19 +7,15 @@ from langchain_core.output_parsers.openai_tools import (
 )
 from langchain_core.messages import HumanMessage
 import datetime
-import os
-from pathlib import Path
-
 
 from dotenv import load_dotenv
 
 load_dotenv()
-load_dotenv(Path(__file__).parent / ".env")
 
 
-llm = ChatOpenAI(model="gpt-4o-mini", api_key=os.environ["OPENAI_API_KEY"])
+llm = ChatOpenAI(model="o4-mini")
 parser = JsonOutputToolsParser(return_id=True)
-# parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
+parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
 
 actor_prompt_template = ChatPromptTemplate.from_messages(
     [
@@ -44,9 +40,9 @@ first_responder_prompt_template = actor_prompt_template.partial(
     first_instruction="Provide a detailed ~250 word answer."
 )
 
-# first_responder = first_responder_prompt_template | llm.bind_tools(
-#     tools=[AnswerQuestion], tool_choice="AnswerQuestion"
-# )
+first_responder = first_responder_prompt_template | llm.bind_tools(
+    tools=[AnswerQuestion], tool_choice="AnswerQuestion"
+)
 
 revise_instructions = """Revise your previous answer using the new information.
     - You should use the previous critique to add important information to your answer.
@@ -59,7 +55,7 @@ revise_instructions = """Revise your previous answer using the new information.
 
 revisor = actor_prompt_template.partial(
     first_instruction=revise_instructions
-) | llm.with_structured_output(ReviseAnswer)
+) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
 
 
 if __name__ == "__main__":
@@ -69,7 +65,8 @@ if __name__ == "__main__":
     )
     chain = (
         first_responder_prompt_template
-        | llm.with_structured_output(AnswerQuestion)
+        | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion")
+        | parser_pydantic
     )
 
     res = chain.invoke(input={"messages": [human_message]})
