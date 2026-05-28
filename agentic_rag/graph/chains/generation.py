b75dd336fc5f6de80dbb06aced8e81859ba411cc
JojuGeorge <jojugeorge001@gmail.com>
2026-05-28 15:09:05 +0530

    generation chain added

---
+++ agentic_rag/graph/chains/generation.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_openai import ChatOpenAI
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+prompt = hub.pull("rlm/rag-prompt")
+
+generation_chain = prompt | llm | StrOutputParser()
