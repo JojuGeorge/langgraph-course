766312cafd12e0a013fce47ad336563880cfe7d3
JojuGeorge <jojugeorge001@gmail.com>
2026-05-28 15:54:32 +0530

    chages in generation file add langchinhub added generation test file

--- agentic_rag/graph/chains/tests/test_chains.py
+++ agentic_rag/graph/chains/tests/test_chains.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from pprint import pprint
 from graph.chains.retrieval_grader import GradeDocument, retrieval_grader
 from ingestion import retriever
+from graph.chains.generation import generation_chain
 
 def test_retrieval_grader_answer_yes() -> None:
     question = "agent memory"
@@ -22,3 +24,11 @@ def test_retrieval_grader_answer_no() -> None:
     res: GradeDocument = retrieval_grader.invoke({"question": question, "document": doc_txt})
     
     assert res.binary_score == "no"
+    
+    
+def test_generation_chain() -> None: 
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    pprint(generation)
