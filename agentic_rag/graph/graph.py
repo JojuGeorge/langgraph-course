ab3368e7400fd6b3594dad449bdc48e59c3958dc
JojuGeorge <jojugeorge001@gmail.com>
2026-05-28 21:40:52 +0530

    added router to graph. DONE

--- agentic_rag/graph/graph.py
+++ agentic_rag/graph/graph.py
@@ -6,6 +6,7 @@ from graph.nodes import generate, grade_documents, retrieve, web_search
 from graph.state import GraphState
 from graph.chains.hallucination_grader import hallucination_grader
 from graph.chains.answer_grader import answer_grader
+from graph.chains.router import question_router, RouteQuery
 
 load_dotenv()
 
@@ -42,15 +43,30 @@ def grade_generation_grounded_in_documents_and_question(state: GraphState) -> st
             return "not useful"
     else:
         return "not supported"
+    
+def route_question(state: GraphState) -> str:
+    print("---ROUTE QUESTION---")
+    question = state["question"]
+    source: RouteQuery = question_router.invoke({"question": question})
+    if source.datasource == WEBSEARCH:
+        print("---ROUTE QUESTION TO WEB SEARCH---")
+        return WEBSEARCH
+    elif source.datasource == "vectorstore":
+        print("---ROUTE QUESTION TO RAG---")
+        return RETRIEVE
             
 
 workflow = StateGraph(GraphState)
 
 workflow.add_node(RETRIEVE, retrieve)
-workflow.set_entry_point(RETRIEVE)
 workflow.add_node(GRADE_DOCUMENTS, grade_documents)
 workflow.add_node(GENERATE, generate)
 workflow.add_node(WEBSEARCH, web_search)
+
+workflow.set_conditional_entry_point(route_question, {
+    WEBSEARCH: WEBSEARCH, 
+    RETRIEVE: RETRIEVE
+})
 
 workflow.add_edge(RETRIEVE, GRADE_DOCUMENTS)
 workflow.add_conditional_edges(
